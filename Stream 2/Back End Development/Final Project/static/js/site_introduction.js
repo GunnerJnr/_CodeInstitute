f779ea4349562cc2ae26e270b3b55ec29dee0b54
David Gunner <gunnerjnr@live.co.uk>
2017-08-07 16:05:31 +0100

    added information for the intro.js tour on each dashboard element

--- Stream 2/Back End Development/Final Project/static/js/site_introduction.js
+++ Stream 2/Back End Development/Final Project/static/js/site_introduction.js
@@ -10,67 +10,71 @@ intro.setOptions({
     steps: [
         {
             element: '#step1',
-            intro: "Click here to reload the dashboard",
+            intro: "Click here to reload the dashboard at any time.",
             position: 'bottom-middle'
         },
         {
             element: '#step2',
-            intro: 'Click here also to reload the dashboard',
+            intro: 'You can also click here to reload the dashboard at any time also.',
             position: 'bottom-middle'
         },
         {
             element: '#year-menu-select',
-            intro: 'TODO',
+            intro: 'This is a drop down list containing the years different Avengers were created/arrived. ' +
+            'Selecting one will filter the data.',
             position: 'middle'
         },
         {
             element: '#name-menu-select',
-            intro: 'TODO',
+            intro: 'This is a drop down list containing the names of the different Avengers. ' +
+            'Selecting one will filter the data.',
             position: 'middle'
         },
         {
             element: '#url-menu-select',
-            intro: 'TODO',
+            intro: 'This list contains urls where information can be found about the different characters, ' +
+            'currently it will just filter the data.',
             position: 'middle'
         },
         {
             element: '#gender-row-chart',
-            intro: 'TODO',
+            intro: 'Here you can see the percentage ratio of Male to Female characters, this can be filtered by clicking on either.',
             position: 'middle'
         },
         {
             element: '#honorary-pie-chart',
-            intro: 'TODO',
+            intro: 'The pie chart contains all the honorary data for the different Avengers. This can be filtered by clicking the slices.',
             position: 'middle'
         },
         {
             element: '#years-nd',
-            intro: 'TODO',
+            intro: 'Displays the total number of years.',
             position: 'middle'
         },
         {
             element: '#appearances-nd',
-            intro: 'TODO',
+            intro: 'Displays the total number of Avenger appearances',
             position: 'middle'
         },
         {
             element: '#year-bar-chart',
-            intro: 'TODO',
+            intro: 'Here is a bar chart displaying the years in a different way, you can drag the mouse over a selection to filter the data.',
             position: 'middle'
         },
         {
             element: '#current-bar-chart',
-            intro: 'TODO',
+            intro: 'Here is a bar chart displaying the current Avengers, you can drag the mouse over a selection to filter the data.',
             position: 'middle'
         },
         {
             element: '#table-wrapper',
-            intro: 'TODO',
+            intro: 'Below you can also view a table displaying different types of information about the named Avengers.',
             position: 'bottom-middle'
         }
     ]
 });
 
 $('#tour-button').click(function () {
-    intro.start()
+    intro.start();
+    $('#tour-button').hide();
 });
