025c3fc2c8cfc02dff38b1089a21fe3a98aeae03
David Gunner <gunnerjnr@live.co.uk>
2017-08-16 10:42:13 +0100

    folder name refactoring for cleaner urls on git

---
+++ Stream_3/Full_Stack_Development/7.Django_Blog_Part_Three/3.Wire_Up_The_Back_To_Posts_Button/challenge_solution/Blog_prj/Blog_app/views.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post
+
+
+# Create your views here.
+def post_list(request):
+    """
+    Create a view that will return a list of
+    Posts that were published prior to 'now' and
+    render them to the 'blogposts.html' template
+    :param request:
+    :return:
+    """
+
+    posts = Post.objects.filter(published_date__lte=timezone.now()
+                                ).order_by('-published_date')
+    return render(request, "blogposts.html", {'posts': posts})
+
+
+def post_detail(request, id):
+    """
+    Create a view that return a single
+    Post object based on the post ID and
+    and render it to the 'postdetail.html'
+    template. Or return a 404 error if the
+    post is not found
+    """
+
+    post = get_object_or_404(Post, pk=id)
+    return render(request, "postdetail.html", {'post': post})
