bad17d9abf3839b2395b42da8a0d77a3b1fb05b7
David Gunner Jnr <gunnerjnr@live.co.uk>
2017-10-05 11:02:26 +0100

    added lesson 2 for Django Rest Framework, in this lesson we created a serializer so that we can fetch/load the todo items and serialize it into a json format using the Rest response call

---
+++ Stream-3/Full-Stack-Development/21.Django REST Framework/2.Serializers-And-Class-Based-Views/django_todo/todo/admin.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from todo.models import Todo
+
+# Register your models here.
+admin.site.register(Todo)
